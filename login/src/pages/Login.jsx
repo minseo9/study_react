5cf052a21570943536920e182b49199a2c0b1b74
minseo9 <kmsals130@gmail.com>
2025-07-08 10:57:04 +0900

    로그인 기능 구현

--- login/src/pages/Login.jsx
+++ login/src/pages/Login.jsx
@@ -1,17 +1,72 @@
 import "../styles/Login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+    const [isLogin, setIsLogin] = useState(false);
+    const loginIdRef = useRef();
+    const loginPwdRef = useRef();
+    const nav = useNavigate();
+
+    const url = import.meta.env.VITE_API_URL;
+
+    const clickLogin = async () => {
+        try {
+            const response = await axios.get(url);
+            const user = response.data;
+
+            const login = user.some(
+                (u) =>
+                    String(u.id) === String(loginIdRef.current.value) &&
+                    String(u.password) === String(loginPwdRef.current.value)
+            );
+
+            console.log(login);
+
+            if (login) {
+                nav("/home", {
+                    state: { id: loginIdRef.current.value },
+                });
+            } else {
+                loginIdRef.current.value = "";
+                loginPwdRef.current.value = "";
+                setIsLogin(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className="login-container">
             <h2>로그인</h2>
-            <form className="login-form">
-                <input type="text" placeholder="아이디" />
-                <input type="password" placeholder="비밀번호" />
+            <form
+                className="login-form"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    clickLogin();
+                }}
+            >
+                <input type="text" placeholder="아이디" ref={loginIdRef} />
+                <input
+                    type="password"
+                    placeholder="비밀번호"
+                    ref={loginPwdRef}
+                />
+                {isLogin && (
+                    <span className="login-error">
+                        아이디 혹은 비밀번호가 잘못됐습니다.
+                    </span>
+                )}
+                <button
+                    type="submit"
+                    className="login-button"
+                    onClick={clickLogin}
+                >
+                    로그인
+                </button>
             </form>
-            <button type="submit" className="login-button">
-                로그인
-            </button>
             <div className="etc-container">
                 <Link to={"/join"} className="join-button">
                     회원가입
